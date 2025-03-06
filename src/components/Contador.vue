1d832e06d9fc0a500d7e12cd5cb1d3ff1b94ace6
Fagoada <syriss@hotmail.com>
2025-03-06 21:15:14 +0100

    Componentes iniciales

---
+++ src/components/Contador.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Hello</h1>
+    <h2>Contador: {{ contador }}</h2>
+    <button @click= "incrementar">+</button>
+    <button @click= "contador--">-</button>
+  </template>
+  
+  <script setup>
+    import { ref } from "vue";
+    const contador = ref(0);
+  
+    const incrementar = () => {
+      contador.value++;
+    }
+  </script>
+  
+  <style scoped>
+  </style>
+  
